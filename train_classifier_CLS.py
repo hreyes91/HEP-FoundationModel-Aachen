34299c6321522da8e7fa8e348a9eba0f40a79cc0
HumbertosMac <you@example.com>
2025-01-30 14:58:34 +0100

    model CLS

--- train_classifier_CLS.py
+++ train_classifier_CLS.py
@@ -99,7 +99,7 @@ def parse_input():
     )
     
     
-        parser.add_argument(
+    parser.add_argument(
         "--num_cls_layers", type=int, default=2, help="Number of transformer layers"
     )
     parser.add_argument(
@@ -250,7 +250,7 @@ if __name__ == "__main__":
     model = JetClassifierWithClassAttention(
         hidden_dim=args.hidden_dim,
         num_layers=args.num_layers,
-            num_cls_layers=args.num_cls_layers,
+        num_cls_layers=args.num_cls_layers,
         num_heads=args.num_heads,
         num_features=num_features,
         dropout=args.dropout,
