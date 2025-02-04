b4fd36ab1f1049067f32f60f2197903161cbf406
HumbertosMac <you@example.com>
2025-02-04 11:19:20 +0100

    class AttPool

--- train_classifier_AttPool.py
+++ train_classifier_AttPool.py
@@ -5,7 +5,7 @@ from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 from argparse import ArgumentParser
 import random
-from model import JetClassifierWithAttentionPoolingCombCLS,JetClassifierWithAttentionPoolingIgCLS(nn.Module)
+from model import JetClassifierWithAttentionPoolingCombCLS,JetClassifierWithAttentionPoolingIgCLS
 
 from tqdm import tqdm
 import pandas as pd
