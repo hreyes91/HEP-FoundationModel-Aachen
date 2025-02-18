a3a72c343a5dc81198128da4bd5d216809da7b49
HumbertosMac <you@example.com>
2025-02-18 12:47:53 +0100

    train_classifier_AL

--- train_classifier_AL_1.py
+++ train_classifier_AL_1.py
@@ -181,7 +181,7 @@ def get_dataloader(bgf,sigf, batch_size=32, shuffle=False, num_workers=4):
     jet2 = torch.tensor(jet2_data[idx], dtype=torch.float32)
     padding_mask1 = torch.tensor(padding_mask1[idx], dtype=torch.bool)
     padding_mask2 = torch.tensor(padding_mask2[idx], dtype=torch.bool)
-    label = torch.tensor(labels[idx], dtype=torch.float32)  # For BCEWithLogitsLoss, labels should be float32
+    label = torch.tensor(label[idx], dtype=torch.float32)  # For BCEWithLogitsLoss, labels should be float32
     
     
     train_set = TensorDataset(
