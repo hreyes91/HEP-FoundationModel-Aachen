f063d5b811f5d74e18d68bc61623a033d0a550dc
HumbertosMac <you@example.com>
2025-02-18 14:32:14 +0100

    train_classifier_AL

--- train_classifier_AL_1.py
+++ train_classifier_AL_1.py
@@ -168,8 +168,8 @@ def get_dataloader(bgf,sigf, batch_size=32, shuffle=False, num_workers=4):
     
     label = np.append(np.zeros(len(bg_jet1)), np.ones(len(sig_jet1)))
     
-    padding_mask1 = jet1_data[:, :, 0] != -1
-    padding_mask2 = jet2_data[:, :, 0] != -1
+    padding_mask1 = jet1_data[:, :, 0] != 0
+    padding_mask2 = jet2_data[:, :, 0] != 0
 
 
 
@@ -182,9 +182,7 @@ def get_dataloader(bgf,sigf, batch_size=32, shuffle=False, num_workers=4):
     padding_mask1 = torch.tensor(padding_mask1[idx], dtype=torch.bool)
     padding_mask2 = torch.tensor(padding_mask2[idx], dtype=torch.bool)
     
-    print(jet2)
-    print(padding_mask2)
-    exit()
+ 
     label = torch.tensor(label[idx], dtype=torch.int64)  # For BCEWithLogitsLoss, labels should be float32
     
     #All should be sme size so I use len(label)
