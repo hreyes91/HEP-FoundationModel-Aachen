85dcb28eacfa03bea1407255ba5de924ef9bfd3d
HumbertosMac <you@example.com>
2025-02-24 16:03:11 +0100

    train_classifier_AL_1

--- train_classifier_scan_Al_finetune.py
+++ train_classifier_scan_Al_finetune.py
@@ -147,7 +147,7 @@ data_path_1=main_dir_discrete+'discrete_bg-N100-SR-Test_1Mfromeach_403030.h5'
 data_path_2=main_dir_discrete+'discrete_sn-N100-SR-Test_1Mfromeach_403030.h5'
 num_events_test=100000
 r_tresh=.3
-dict_auc={'suffix':[],'num_events':[],'auc':[],'r_'+str(r_tresh):[],'acc':[]}
+dict_auc={'suffix':[],'sig':[],'auc':[],'r_'+str(r_tresh):[],'acc':[]}
 
 
 
