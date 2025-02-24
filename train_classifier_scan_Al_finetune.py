33eb02a0be4fa19b3a75e4d07972e5bc332b3a30
HumbertosMac <you@example.com>
2025-02-24 15:13:13 +0100

    test AL

--- train_classifier_scan_Al_finetune.py
+++ train_classifier_scan_Al_finetune.py
@@ -19,6 +19,123 @@ def random_string():
     print("The generated random string : " + str(res))
     return str(res)
     
+    
+    
+def read_file(file_name):
+
+    f = open(file_name, "r")
+    lines = f.readlines()
+
+    return lines
+
+
+def extract_value(var,lines):
+
+    for line in lines:
+        if 'lr_' in line:
+            continue
+        if var in line:
+            line=line.replace(' ','')
+            line=line.replace('\n','')
+
+            value=line.split(var)[-1]
+    
+    return value
+
+
+def GetPredictions(model_dir):
+
+    predicitions_test = np.load(model_dir+'/predictions_test.npz')
+    
+    
+    return predicitions_test
+    
+
+
+def GetPredictions(model_dir):
+
+    predicitions_test = np.load(model_dir+'/predictions_test.npz')
+    
+    return predicitions_test
+
+
+
+
+def ComputeR(tpr,fpr,r_tresh):
+    # Compute ROC curve and ROC area for each class
+    
+
+    results= np.stack((tpr, 1/fpr), -1)
+    
+    results_pandas=pd.DataFrame(results,columns=['tpr','1/fpr'])
+    
+    print(results)
+    print(results_pandas)
+    threshold = r_tresh
+
+    # Select rows where 'Column 1' values are above the threshold
+    filtered_df = results_pandas[results_pandas['tpr'] < threshold]
+    print(filtered_df)
+    r_val=float(filtered_df.sort_values(by=['tpr'],ascending=False).reset_index()[['1/fpr']].iloc[0])
+
+    return r_val
+
+
+def Accuracy(predictions,labels):
+
+    fixed_pred=predictions
+    fixed_pred[fixed_pred >= 0.5] = 1
+    fixed_pred[fixed_pred < 0.5] = 0
+
+    acc=accuracy_score(labels, fixed_pred,normalize=True)
+    
+    return acc
+
+def TestMetrics(predictions,r_tresh):
+
+    
+
+    labels=predictions['labels']
+    predictions=predictions['predictions']
+
+    fpr, tpr, _ = roc_curve(labels, predictions)
+    
+    
+    auc_score=roc_auc_score(labels, predictions)
+    
+    
+    r_tresh=.5
+    r_val=ComputeR(tpr,fpr,r_tresh)
+    
+    
+    acc=Accuracy(predictions,labels)
+
+    return auc_score,r_val,acc
+
+
+
+
+def TestResults(dict_auc,model_dir,r_tresh):
+
+
+    arguments_file=read_file(model_dir+'/arguments.txt')
+    num_events_train=extract_value('num_events',arguments_file)
+    print(num_events_train)
+
+
+    predictions=GetPredictions(model_dir)
+    auc_score,r_val,acc=TestMetrics(predictions,r_tresh)
+
+    dict_auc.get('num_events').append(num_events_train)
+    dict_auc.get('auc').append(auc_score)
+    dict_auc.get('r_'+str(r_tresh)).append(r_val)
+    dict_auc.get('acc').append(acc)
+  
+
+    return
+
+
+
 main_dir_discrete='/net/data_ttk/hreyes/LHCO/LHCO_discrete/'
 
 sig_list=['discrete_Weak-mix-Train-10000_1Mfromeach_403030.h5']
