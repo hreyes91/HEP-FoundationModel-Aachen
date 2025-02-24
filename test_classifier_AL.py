f8a8f482324dc3cffbb6e69f1fb848b296dc2784
HumbertosMac <you@example.com>
2025-02-24 15:14:16 +0100

    test AL

--- test_classifier_AL.py
+++ test_classifier_AL.py
@@ -135,7 +135,7 @@ def get_dataloader(bgf,sigf, batch_size=32, shuffle=False, num_workers=4):
     bg_jet1,bg_jet2 = load_data(bgf)
     sig_jet1,sig_jet2 = load_data(sigf)
 
-    print(f"Using bg {bg.shape} from {bgf} and sig {sig.shape} from {sigf}")
+    print(f"Using bg {bg_jet1.shape} from {bg_jet1} and sig {sig_jet1.shape} from {sig_jet1}")
 
     jet1_data = np.concatenate((bg_jet1, sig_jet1), 0)
     jet2_data = np.concatenate((bg_jet2, sig_jet2), 0)
