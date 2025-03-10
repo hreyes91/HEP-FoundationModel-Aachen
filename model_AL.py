c71465220fb62af122cd627923ed980b5209d4c6
HumbertosMac <you@example.com>
2025-03-10 12:03:01 +0100

    class AL wHLF

--- model_AL.py
+++ model_AL.py
@@ -254,8 +254,8 @@ class JetTransformerALwHLF(Module):
 
         #####HLF stage
         hlf = hlf.view(hlf.shape[0], -1)
-        print(hlf)
-        print(hlf.shape)
+        #print(hlf)
+        #print(hlf.shape)
         hlf=self.hlf_mlp1(hlf)
         hlf = torch.nn.LeakyReLU(negative_slope=0.01)(hlf)
         hlf = self.hlf_transformer(hlf) # Transformer
