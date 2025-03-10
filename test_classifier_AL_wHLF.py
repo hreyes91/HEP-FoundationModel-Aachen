44b94a9460ead365008a79a84893f9b95bc2341b
HumbertosMac <you@example.com>
2025-03-10 12:28:27 +0100

    train_classifier_AL_wHLF

--- test_classifier_AL_wHLF.py
+++ test_classifier_AL_wHLF.py
@@ -148,7 +148,7 @@ def get_dataloader(bgf,sigf, batch_size=32, shuffle=False, num_workers=4):
     padding_mask2 = jet2_data[:, :, 0] != 0
 
 
-    jet_coords = torch.tensor(jet_coords[idx], dtype=torch.bool)
+    jet_coords = torch.tensor(jet_coords_data[idx], dtype=torch.bool)
 
 
     #idx = np.random.permutation(len(label))
