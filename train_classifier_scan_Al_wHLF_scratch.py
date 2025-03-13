3875a74148d75b1b8ea66abd0b0be8320751043b
HumbertosMac <you@example.com>
2025-03-13 16:21:23 +0100

    class AL all

--- train_classifier_scan_Al_wHLF_scratch.py
+++ train_classifier_scan_Al_wHLF_scratch.py
@@ -141,9 +141,9 @@ def TestResults(dict_auc,model_dir,r_tresh):
 
 main_dir_discrete='/net/data_ttk/hreyes/LHCO/LHCO_discrete/'
 
-sig_list=['discrete_1Mfromeach_403030_Weak-mix-Train-10000.h5','discrete_1Mfromeach_403030_Weak-mix-Train-5000.h5','discrete_1Mfromeach_403030_Weak-mix-Train-2000.h5','discrete_1Mfromeach_403030_Weak-mix-Train-1000.h5','discrete_1Mfromeach_403030_Weak-mix-Train-600.h5','discrete_1Mfromeach_403030_Weak-mix-Train-300.h5','discrete_1Mfromeach_403030_Weak-mix-Train-100.h5']
+#sig_list=['discrete_1Mfromeach_403030_Weak-mix-Train-10000.h5','discrete_1Mfromeach_403030_Weak-mix-Train-5000.h5','discrete_1Mfromeach_403030_Weak-mix-Train-2000.h5','discrete_1Mfromeach_403030_Weak-mix-Train-1000.h5','discrete_1Mfromeach_403030_Weak-mix-Train-600.h5','discrete_1Mfromeach_403030_Weak-mix-Train-300.h5','discrete_1Mfromeach_403030_Weak-mix-Train-100.h5']
 
-#sig_list=['discrete_1Mfromeach_403030_Weak-mix-Train-10000.h5']
+sig_list=['discrete_1Mfromeach_403030_Weak-mix-Train-10000.h5']
 
 bg_list=['discrete_1Mfromeach_403030_bg-N100-SR-Train.h5']
 
