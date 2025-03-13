222d1a1ff4edcd83b580da077634610c6e15700c
HumbertosMac <you@example.com>
2025-03-13 17:07:58 +0100

    class AL supervised

---
+++ train_classifier_AL_supervised_all.py
@@ -0,0 +1,7 @@
+import os
+
+
+
+os.system('python train_classifier_scan_Al_wHLF_scratch_supervised.py')
+os.system('python train_classifier_scan_Al_wHLF_finetune_supervised.py')
+os.system('python train_classifier_scan_Al_finetune_supervised.py')
