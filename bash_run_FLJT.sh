5be7d48ff72d1ee5ffc71fd8e72d0b6eb25a5ef5
hreyes91 <37078103+hreyes91@users.noreply.github.com>
2026-02-03 19:02:58 -0600

    Create bash_run_FLJT.sh

---
+++ bash_run_FLJT.sh
@@ -0,0 +1,34 @@
+
+#!/bin/bash
+#Activate the Python environment
+#source /home/home3/institut_thp/hreyes/anaconda3/envs/torch_env_pip/bin/activate 
+
+source /home/home3/institut_thp/hreyes/anaconda3/etc/profile.d/conda.sh
+
+# Activate your environment
+conda activate torch_env_pip
+echo "environmet activated"
+#Run the Python script
+
+cd /home/home3/institut_thp/hreyes/Transformers/FoundationModels/HEP-FoundationModel-Aachen
+
+run="RUN101"
+
+echo "found the directory. Starting training"
+
+python train.py --data_path /net/data_ttk/hreyes/OneBin/TTBar_train___1Mfromeach_403030.h5 --model_path  /net/data_ttk/hreyes/FLJT/ResultsV2/Tests_TOP --log_dir /net/data_ttk/hreyes/FLJT/ResultsV2/Tests_TOP  --output linear --num_const 128 --num_epochs 36  --lr 0.001 --lr_decay 1e-06 --batch_size 100 --num_events 10000000 --dropout 0 --num_heads 4 --num_layers 8 --num_bins 41 31 31 --weight_decay 1e-05 --hidden_dim 256 --end_token --start_token  --name_sufix ${run} --num_events_val 500000 --checkpoint_steps 1200000
+
+echo "Tranning done."
+myArray=(0 1 5 10 15 20 25 30 35)
+
+for str in "${myArray[@]}"; do
+    echo "Running training with $str events..."
+
+ 
+
+    echo "Sampling jets for $str..."
+    python sample_jets.py --model_dir  /net/data_ttk/hreyes/FLJT/ResultsV2/Tests_TOP_${run}/ --savetag epoch_${str}  --num_samples 100 --num_const 128 --trunc 5000 --batchsize 100 --model_name model_epoch_${str}.pt
+    echo "density estimation jets for $str..."
+    python evaluate_probabilities.py --model  /net/data_ttk/hreyes/FLJT/ResultsV2/Tests_TOP_${run}/model_epoch_${str}.pt  --data /net/data_ttk/hreyes/FLJT/ResultsV2/Tests_TOP_${run}/samples_epoch_${str}.h5 --tag eval_samples_epoch_${str} --num_const 128  --num_events 100 --fixed_samples
+
+done
